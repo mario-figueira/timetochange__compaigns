482fce337c4115610998f663049031381dfa3d55
Mario Figueira <mario.figueira@gtinformatica.eu>
2013-02-27 11:17:10 +0000

    header.php improvement

--- views/header.php
+++ views/header.php
@@ -38,18 +38,67 @@
 	</head>
 
 
-	<body id="all_container" class="browser_center">
+	<body>
 		
-		<div class="clear"></div>
-			
-
-		<div id="app_header" class="">
-			«HEADER»
-		</div>
+		<!-- <div class="clear"></div> -->
 		
-		<div id="app_body" class="">
+		<div id="container" class="container">
 <!-- file header.php - END -->
 
+<?php
+	$transparent_img_url = $this->build_img_url("transparent.png");
+	
+	
+	require_once REALPATH ."enums/menu_items_enum.php";
+	
+	$dashboard_classes = "";
+	$campaigns_mgnt_classes = "";
+	$explorer_classes = "";
+	$settings_classes = "";
+	$profile_classes = "";
+	$logout_classes = "";
+	
+	
+	//$selected_menu = menus_items_enum::$C_CAMPAIGNS; //hardcoded;
+	
+	switch($selected_menu){
+		case menus_items_enum::$C_DASHBOARD;
+			$dashboard_classes = "selected";
+			break;
+		case menus_items_enum::$C_CAMPAIGNS_MGNT;
+			$campaigns_mgnt_classes = "selected";
+			break;
+		case menus_items_enum::$C_EXPLORER;
+			$explorer_classes = "selected";
+			break;
+		case menus_items_enum::$C_SETTINGS;
+			$settings_classes = "selected";
+			break;
+		case menus_items_enum::$C_PROFILE;
+			$profile_classes = "selected";
+			break;
+		case menus_items_enum::$C_LOGOUT;
+			$logout_classes = "selected";
+			break;
+		default:
+				throw new Exception("Unsuported case:[{$selected_menu}].");		
+			break;
+	}
+	
+	
+	//$menu_item_campaigns_mgnt_action_url = BASEPATH ."main_menu/{$
+?>
+			<div class="nav">
+				<ul>
+					<li><a class="<?php echo $dashboard_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_DASHBOARD); ?>" title="Dashboard"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Dashboard" class="icoDashboard"></a></li>
+					<li><a class="<?php echo $campaigns_mgnt_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_CAMPAIGNS_MGNT); ?>" title="Campaign Management"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Campaign Management" class="icoDatabase"></a></li>   
+					<li><a class="<?php echo $explorer_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_EXPLORER); ?>" title="File Explorer"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="File Explorer" class="icoShoebox"></a></li>    
+					<li><a class="<?php echo $settings_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_SETTINGS); ?>" title="Settings"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Settings" class="icoGear"></a></li>
+					<li><a class="<?php echo $profile_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_PROFILE); ?>" title="Profile"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Profile" class="icoUser"></a></li>
+					<li><a class="<?php echo $logout_classes;?>" href="<?php echo $this->build_select_menu_action_url(menus_items_enum::$C_LOGOUT); ?>" title="Logout"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Logout" class="icoCirclex"></a></li>
+				</ul>
+				<div class="logoT2C"><img src="<?php echo $transparent_img_url; ?>" width="1" height="1" alt="Time 2 Change" class="icoT2C"></div>
+			</div>
 
 
 
