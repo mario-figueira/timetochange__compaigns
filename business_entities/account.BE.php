9a2eab80e30474cb814c6ae2680b09e98868f2b1
Mario Figueira <mario.figueira@gtinformatica.eu>
2013-02-27 18:13:51 +0000

    account__BE: mail validation added

--- business_entities/account.BE.php
+++ business_entities/account.BE.php
@@ -6,9 +6,16 @@ class account__BE extends base__BE {
 
 	protected $idAccount;
 	protected $name;
+	protected $email;
+	protected $zip1;
+	protected $zip2;
 
 	protected function __construct($a_fields_array) {
 		parent::__construct($a_fields_array);
+		
+		//parent::register_validation("email", array($this, 'string__is_valid_email'), "email is inválid");
+		//parent::register_validation("email", array($this, 'string__is_valid_email'), "email is not valid");
+		//parent::register_validation("name", array($this, 'name_is_valid'), "name is not valid");
 	}
 
 	public static function create_from_record($a_business_entity_record) {
@@ -28,15 +35,37 @@ class account__BE extends base__BE {
 		return $ret_val;
 	}
 
-	/*
-	  protected function name__get(){
-	  return  "name=" .$this->name;
-	  }
-	 */
 
-	/*
-	  protected function name__set($a_value){
-	  $this->name = $a_value;
-	  }
-	 */
+	protected function email__set($a_value) {
+		
+		$value_is_set = isset($a_value);
+		
+		if($value_is_set){
+			$evaluation_result = $this->value_is_valid_for_field($a_value, "email");
+			$value_is_valid = $evaluation_result['is_valid'];
+			if (!$value_is_valid) {
+				$is_invalid_message = $evaluation_result['invalid_messages'];
+				throw new Exception("Parameter a_value=[{$a_value}] is not a valid email.");
+			}
+		}
+		$this->email = $a_value;
+	}
+
+	protected function string__is_valid_email($a_email_address_str) {
+		$ret_val = false;
+
+		$ret_val = (filter_var($a_email_address_str, FILTER_VALIDATE_EMAIL) != false);
+
+		return $ret_val;
+	}
+
+	protected function name_is_valid($a_name) {
+		$ret_val = false;
+
+		$ret_val = strlen($a_name)>100;
+
+		return $ret_val;
+	}
+
+
 }
