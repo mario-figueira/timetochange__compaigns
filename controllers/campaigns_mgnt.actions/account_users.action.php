7e4b539709961331731319aa70db035f310ec03e
Mario Figueira <mario.figueira@gtinformatica.eu>
2013-02-28 18:28:12 +0000

    account_users function of selected account

--- controllers/campaigns_mgnt.actions/account_users.action.php
+++ controllers/campaigns_mgnt.actions/account_users.action.php
@@ -6,14 +6,24 @@ class account_usersAction {
 	public function execute($a_command){
 		require_once REALPATH ."/repositories/repository.FACTORY.php";
 		
+		$selected = $_POST['selected'];
+		
+		$no_selected_accounts = !isset($selected);
+		if($no_selected_accounts){
+			return;
+		}
+		
+		$account_id_to_show_users = $selected[0];
+
+		
+		
 		$repo_factory = new repository__FACTORY();
 		
 		$accounts_repo = $repo_factory->get_repository_by_business_entity_name("account");
 		
-		$a_account_id = $a_command->get_parameter("account_id");
 		
 		
-		$account_users = $accounts_repo->users_of_account__get_by_account_id($a_account_id);
+		$account_users = $accounts_repo->users_of_account__get_by_account_id($account_id_to_show_users);
 		
 		$a_command->set_parameter("account_users", $account_users);
 		
