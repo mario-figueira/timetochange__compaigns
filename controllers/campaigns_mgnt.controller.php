9579df12e6af32a0f964a6777429c0efbd085fc8
Mario Figueira <mario.figueira@gtinformatica.eu>
2013-03-01 16:54:04 +0000

    automagically call a action class execute method refactoring

--- controllers/campaigns_mgnt.controller.php
+++ controllers/campaigns_mgnt.controller.php
@@ -14,36 +14,23 @@ class campaigns_mgntController extends defaultController {
 
 	public function _accounts(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/accounts.action.php";
-		
-		$action = new accountsAction();
-		$action->execute($this->Command);
-	
+		$this->execute_action_class();
 		
 		parent::_defaultAction();
 	}
 	
 	public function _add_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/add_account.action.php";
-		
-		$action = new add_accountAction();
-		$action->execute($this->Command);
-	
+		$this->execute_action_class();	
 		
 		parent::_defaultAction();
 	}
 	
 	public function _save_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/save_account.action.php";
-		
-		$action = new save_accountAction();
-		$action->execute($this->Command);
-	
-		
-		$this->redirect_to_controller_action("campaigns_mgnt", "accounts");
-		
+		$this->execute_action_class();
+			
+		$this->redirect_to_controller_action("campaigns_mgnt", "accounts");		
 	}
 	
 	
@@ -51,14 +38,9 @@ class campaigns_mgntController extends defaultController {
 	
 	public function _edit_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/edit_account.action.php";
-		
-		$action = new edit_accountAction();
-		$action->execute($this->Command);
-	
-		
-		parent::_defaultAction();
-		
+		$this->execute_action_class();
+			
+		parent::_defaultAction();		
 	}
 	
 	
@@ -66,60 +48,39 @@ class campaigns_mgntController extends defaultController {
 	
 	public function _account_users(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/account_users.action.php";
-		
-		$action = new account_usersAction();
-		$action->execute($this->Command);	
-	
-		
+		$this->execute_action_class();
+			
 		parent::_defaultAction();
 	}
 
 	
 	public function _delete_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/delete_account.action.php";
+		$this->execute_action_class();	
 		
-		$action = new delete_accountAction();
-		$action->execute($this->Command);
-	
-		
-		$this->redirect_to_controller_action("campaigns_mgnt", "accounts");
-		
+		$this->redirect_to_controller_action("campaigns_mgnt", "accounts");		
 	}
 	
 	public function _add_user_to_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/add_user_to_account.action.php";
-		
-		$action = new add_user_to_accountAction();
-		$action->execute($this->Command);	
-	
-		
+		$this->execute_action_class();
+			
 		parent::_defaultAction();
 	}
 	
 	public function _edit_account_user(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/edit_account_user.action.php";
-		
-		$action = new edit_account_userAction();
-		$action->execute($this->Command);	
-	
-		
+		$this->execute_action_class();
+			
 		parent::_defaultAction();
 	}
 
 	public function _save_user_to_account(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/save_user_to_account.action.php";
-		
-		$action = new save_user_to_accountAction();
-		$action->execute($this->Command);	
+		$this->execute_action_class();
 	
 		$idAccount = $this->Command->get_parameter("idAccount");
 		$this->redirect_to_controller_action("campaigns_mgnt", "account_users", "idAccount/{$idAccount}");
-
 	}
 	
 	
@@ -127,59 +88,36 @@ class campaigns_mgntController extends defaultController {
 	
 	public function _campaigns(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/campaigns.action.php";
-		
-		$action = new campaignsAction();
-		$action->execute($this->Command);
-	
-		
+		$this->execute_action_class();
+				
 		parent::_defaultAction();
 	}
 	
 	public function _delete_campaign(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/delete_campaign.action.php";
-		
-		$action = new delete_campaignAction();
-		$action->execute($this->Command);
-	
-		
-		$this->redirect_to_controller_action("campaigns_mgnt", "campaigns");
-		
+		$this->execute_action_class();
+			
+		$this->redirect_to_controller_action("campaigns_mgnt", "campaigns");		
 	}
 	
 	public function _add_campaign(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/add_campaign.action.php";
-		
-		$action = new add_campaignAction();
-		$action->execute($this->Command);
-	
-		
+		$this->execute_action_class();
+			
 		parent::_defaultAction();
 	}
 	
 	public function _save_campaign(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/save_campaign.action.php";
+		$this->execute_action_class();
 		
-		$action = new save_campaignAction();
-		$action->execute($this->Command);
-	
-		
-		$this->redirect_to_controller_action("campaigns_mgnt", "campaigns");
-		
+		$this->redirect_to_controller_action("campaigns_mgnt", "campaigns");		
 	}
 	
 	public function _edit_campaign(){
 		
-		require_once REALPATH ."/controllers/campaigns_mgnt.actions/edit_campaign.action.php";
+		$this->execute_action_class();
 		
-		$action = new edit_campaignAction();
-		$action->execute($this->Command);
-	
-		
-		parent::_defaultAction();
-		
+		parent::_defaultAction();		
 	}
 }
