f7fabcb77b06bad3fb9093d747a3d3a336b34eb7
andre.aguiar <andre.aguiar@gtinformatica.eu>
2013-03-04 17:33:49 +0000

    trucate string with elipsis

--- views/explorer/prompts.php
+++ views/explorer/prompts.php
@@ -59,6 +59,7 @@ foreach ($prompts as $prompt){
 	if($description_is_empty){
 		$description = "&nbsp";
 	}
+	$description = truncate_with_elipsis($description,30,true);
 ?>
 	<ul class="row">
         	<label>
@@ -80,4 +81,21 @@ foreach ($prompts as $prompt){
     
     <!--End of List Table Example-->
     
-    
+
+<?php
+function truncate_with_elipsis($string, $length, $stopanywhere=false) {
+    //truncates a string to a certain char length, stopping on a word if not specified otherwise.
+    if (strlen($string) > $length) {
+        //limit hit!
+        $string = substr($string,0,($length -3));
+        if ($stopanywhere) {
+            //stop anywhere
+            $string .= '...';
+        } else{
+            //stop on a word.
+            $string = substr($string,0,strrpos($string,' ')).'...';
+        }
+    }
+    return $string;
+}
+?>
